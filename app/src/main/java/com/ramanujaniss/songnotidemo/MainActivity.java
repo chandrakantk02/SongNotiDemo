f43131b40f0e5305ec6c7ad039466d1d98df9136
chandrakantk02 <chandrakantk02@gmail.com>
2017-09-13 12:24:29 +0530

    Initial commit

---
+++ app/src/main/java/com/ramanujaniss/songnotidemo/MainActivity.java
@@ -0,0 +1,26 @@
+package com.ramanujaniss.songnotidemo;
+
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+
+public class MainActivity extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+
+
+    public void startService(View v) {
+        Intent serviceIntent = new Intent(MainActivity.this, NotificationService.class);
+        serviceIntent.setAction(Constants.ACTION.STARTFOREGROUND_ACTION);
+        startService(serviceIntent);
+    }
+}
